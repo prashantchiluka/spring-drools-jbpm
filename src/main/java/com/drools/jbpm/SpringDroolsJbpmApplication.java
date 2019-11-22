36810adb0f4eddf58be77e0918193b3b259d0a35
prashantchiluka <prashantchiluka@github.com>
2019-11-22 12:31:26 +0530

    initial commit

---
+++ src/main/java/com/drools/jbpm/SpringDroolsJbpmApplication.java
@@ -0,0 +1,13 @@
+package com.drools.jbpm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDroolsJbpmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDroolsJbpmApplication.class, args);
+	}
+
+}
